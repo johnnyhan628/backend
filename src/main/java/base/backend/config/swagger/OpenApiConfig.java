d80956da96a1400fd0c5bcd023cbbee0c48d97ea
johnnyhan628 <johnnyhan628@gmail.com>
2025-07-23 16:58:07 +0900

    Fix: import java.util.List for swagger config

--- src/main/java/base/backend/config/swagger/OpenApiConfig.java
+++ src/main/java/base/backend/config/swagger/OpenApiConfig.java
@@ -1,5 +1,7 @@
 package base.backend.config.swagger;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
